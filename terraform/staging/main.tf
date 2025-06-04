dccc8d7110deec883142aad7fc86f3e8a618530a
ThomasCardin <thomas.cardin@inspection.gc.ca>
2025-06-04 09:37:58 -0400

    issue #201: terraform init config

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1,13 +1,16 @@
 terraform {
   backend "s3" {
-    endpoint                    = "https://minio.inspection.alpha.canada.ca"
     bucket                      = "terraform-staging-state"
     key                         = "tfstagingstate.tfstate"
+    region                      = "main"
     skip_credentials_validation = true # Skip AWS related checks and validations
     skip_requesting_account_id  = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     force_path_style            = true
+    endpoint = {
+      s3 = "https://minio.inspection.alpha.canada.ca"
+    }
   }
 }
 
